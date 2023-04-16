57c7aaf7d6f0d7b659e956fb0d9bae588ed6ad58
Tatsuru Kikuchi <58812919+Tatsuru-Kikuchi@users.noreply.github.com>
2023-04-16 21:27:40 +0900

    Add files via upload

--- ogjpn/calibrate.py
+++ ogjpn/calibrate.py
@@ -1,11 +1,9 @@
-from ogjpn import estimate_beta_j, bequest_transmission, demographics
+from ogjpn import estimate_beta_j, demographics
 from ogjpn import macro_params, transfer_distribution, income
-from ogjpn import get_micro_data, psid_data_setup
 import os
 import numpy as np
 from ogcore import txfunc
 from ogcore.utils import safe_read_pickle, mkdirs
-import pkg_resources
 
 
 class Calibration:
@@ -24,14 +22,11 @@ class Calibration:
         client=None,
         num_workers=1,
     ):
+
         self.estimate_tax_functions = estimate_tax_functions
         self.estimate_beta = estimate_beta
         self.estimate_chi_n = estimate_chi_n
         if estimate_tax_functions:
-            if tax_func_path is not None:
-                run_micro = False
-            else:
-                run_micro = True
             self.tax_function_params = self.get_tax_function_parameters(
                 p,
                 iit_reform,
@@ -39,11 +34,11 @@ class Calibration:
                 data,
                 client,
                 num_workers,
-                run_micro=run_micro,
+                run_micro=True,
                 tax_func_path=tax_func_path,
             )
-        if estimate_beta:
-            self.beta_j = estimate_beta_j.beta_estimate(self)
+        # if estimate_beta:
+        #     self.beta_j = estimate_beta_j.beta_estimate(self)
         # if estimate_chi_n:
         #     chi_n = self.get_chi_n()
 
@@ -51,17 +46,26 @@ class Calibration:
         self.macro_params = macro_params.get_macro_params()
 
         # eta estimation
-        self.eta = transfer_distribution.get_transfer_matrix()
+        # self.eta = transfer_distribution.get_transfer_matrix()
 
         # zeta estimation
-        self.zeta = bequest_transmission.get_bequest_matrix()
+        # self.zeta = bequest_transmission.get_bequest_matrix()
 
         # demographics
         self.demographic_params = demographics.get_pop_objs(
-            p.E, p.S, p.T, 1, 100, p.start_year
+            p.E,
+            p.S,
+            p.T,
+            1,
+            100,
+            p.start_year - 1,
+            p.start_year,
+            GraphDiag=True,
         )
         # demographics for 80 period lives (needed for getting e below)
-        demog80 = demographics.get_pop_objs(20, 80, p.T, 1, 100, p.start_year)
+        demog80 = demographics.get_pop_objs(
+            20, 80, p.T, 1, 100, p.start_year - 1, p.start_year
+        )
 
         # earnings profiles
         self.e = income.get_e_interp(
@@ -87,17 +91,14 @@ class Calibration:
         """
         Reads pickle file of tax function parameters or estimates the
         parameters from microsimulation model output.
-
         Args:
             client (Dask client object): client
             run_micro (bool): whether to estimate parameters from
                 microsimulation model
             tax_func_path (string): path where find or save tax
                 function parameter estimates
-
         Returns:
             None
-
         """
         # set paths if none given
         if tax_func_path is None:
@@ -119,9 +120,8 @@ class Calibration:
             dict_params, run_micro = self.read_tax_func_estimate(
                 p, tax_func_path
             )
-            taxcalc_version = "Cached tax parameters, no taxcalc version"
         if run_micro:
-            micro_data, taxcalc_version = get_micro_data.get_data(
+            micro_data, _ = get_micro_data.get_data(
                 baseline=p.baseline,
                 start_year=p.start_year,
                 reform=iit_reform,
@@ -154,80 +154,139 @@ class Calibration:
             np.ones(p.T + p.S - p.BW)
             * dict_params["tfunc_frac_tax_payroll"][-1],
         )
-        # Conduct checks to be sure tax function params are consistent
-        # with the model run
+
+        # Reorder indices of tax function and tile for all years after
+        # budget window ends
+        num_etr_params = dict_params["tfunc_etr_params_S"].shape[2]
+        num_mtrx_params = dict_params["tfunc_mtrx_params_S"].shape[2]
+        num_mtry_params = dict_params["tfunc_mtry_params_S"].shape[2]
+        # First check to see if tax parameters that are used were
+        # estimated with a budget window and ages that are as long as
+        # the those implied based on the start year and model age.
+        # N.B. the tax parameters dictionary does not save the years
+        # that correspond to the parameter estimates, so the start year
+        # used there may name match what is used in a run that reads in
+        # some cached tax function parameters.  Likewise for age.
         params_list = ["etr", "mtrx", "mtry"]
-        BW_in_tax_params = dict_params["BW"]
-        start_year_in_tax_params = dict_params["start_year"]
-        S_in_tax_params = len(dict_params["tfunc_etr_params_S"][0])
-        # Check that start years are consistent in model and cached tax functions
-        if p.start_year != start_year_in_tax_params:
+        BW_in_tax_params = dict_params["tfunc_etr_params_S"].shape[1]
+        S_in_tax_params = dict_params["tfunc_etr_params_S"].shape[0]
+        if p.BW != BW_in_tax_params:
             print(
-                "Input Error: There is a discrepancy between the start"
+                "Warning: There is a discrepency between the start"
                 + " year of the model and that of the tax functions!!"
             )
-            assert False
-        # Check that S is consistent in model and cached tax functions
-        # Note: even if p.age_specific = False, the arrays coming from
-        # ogcore.txfunc_est should be of length S
+        # After printing warning, make it work by tiling
+        if p.BW > BW_in_tax_params:
+            for item in params_list:
+                dict_params["tfunc_" + item + "_params_S"] = np.concatenate(
+                    (
+                        dict_params["tfunc_" + item + "_params_S"],
+                        np.tile(
+                            dict_params["tfunc_" + item + "_params_S"][
+                                :, -1, :
+                            ].reshape(S_in_tax_params, 1, num_etr_params),
+                            (1, p.BW - BW_in_tax_params, 1),
+                        ),
+                    ),
+                    axis=1,
+                )
+                dict_params["tfunc_avg_" + item] = np.append(
+                    dict_params["tfunc_avg_" + item],
+                    np.tile(
+                        dict_params["tfunc_avg_" + item][-1],
+                        (p.BW - BW_in_tax_params),
+                    ),
+                )
         if p.S != S_in_tax_params:
             print(
-                "Input Error: There is a discrepancy between the ages"
+                "Warning: There is a discrepency between the ages"
                 + " used in the model and those in the tax functions!!"
             )
-            assert False
-
-        # Extrapolate tax function parameters for years after budget window
-        # list of list: BW x S - either an array of function at that element...
-        etr_params = [[None] * p.S] * p.T
-        mtrx_params = [[None] * p.S] * p.T
-        mtry_params = [[None] * p.S] * p.T
-        for s in range(p.S):
-            for t in range(p.T):
-                if t < p.BW:
-                    etr_params[t][s] = dict_params["tfunc_etr_params_S"][t][s]
-                    mtrx_params[t][s] = dict_params["tfunc_mtrx_params_S"][t][
-                        s
-                    ]
-                    mtry_params[t][s] = dict_params["tfunc_mtry_params_S"][t][
-                        s
-                    ]
-                else:
-                    etr_params[t][s] = dict_params["tfunc_etr_params_S"][-1][s]
-                    mtrx_params[t][s] = dict_params["tfunc_mtrx_params_S"][-1][
-                        s
-                    ]
-                    mtry_params[t][s] = dict_params["tfunc_mtry_params_S"][-1][
-                        s
-                    ]
+        # After printing warning, make it work by tiling
+        if p.S > S_in_tax_params:
+            for item in params_list:
+                dict_params["tfunc_" + item + "_params_S"] = np.concatenate(
+                    (
+                        dict_params["tfunc_" + item + "_params_S"],
+                        np.tile(
+                            dict_params["tfunc_" + item + "_params_S"][
+                                -1, :, :
+                            ].reshape(1, p.BW, num_etr_params),
+                            (p.S - S_in_tax_params, 1, 1),
+                        ),
+                    ),
+                    axis=0,
+                )
+        etr_params = np.empty((p.T, p.S, num_etr_params))
+        mtrx_params = np.empty((p.T, p.S, num_mtrx_params))
+        mtry_params = np.empty((p.T, p.S, num_mtry_params))
+        etr_params[: p.BW, :, :] = np.transpose(
+            dict_params["tfunc_etr_params_S"][: p.S, : p.BW, :], axes=[1, 0, 2]
+        )
+        etr_params[p.BW :, :, :] = np.tile(
+            np.transpose(
+                dict_params["tfunc_etr_params_S"][: p.S, -1, :].reshape(
+                    p.S, 1, num_etr_params
+                ),
+                axes=[1, 0, 2],
+            ),
+            (p.T - p.BW, 1, 1),
+        )
+        mtrx_params[: p.BW, :, :] = np.transpose(
+            dict_params["tfunc_mtrx_params_S"][: p.S, : p.BW, :],
+            axes=[1, 0, 2],
+        )
+        mtrx_params[p.BW :, :, :] = np.transpose(
+            dict_params["tfunc_mtrx_params_S"][: p.S, -1, :].reshape(
+                p.S, 1, num_mtrx_params
+            ),
+            axes=[1, 0, 2],
+        )
+        mtry_params[: p.BW, :, :] = np.transpose(
+            dict_params["tfunc_mtry_params_S"][: p.S, : p.BW, :],
+            axes=[1, 0, 2],
+        )
+        mtry_params[p.BW :, :, :] = np.transpose(
+            dict_params["tfunc_mtry_params_S"][: p.S, -1, :].reshape(
+                p.S, 1, num_mtry_params
+            ),
+            axes=[1, 0, 2],
+        )
 
         if p.constant_rates:
             print("Using constant rates!")
-            # Make all tax rates equal the average
-            p.tax_func_type = "linear"
-            etr_params = [[None] * p.S] * p.T
-            mtrx_params = [[None] * p.S] * p.T
-            mtry_params = [[None] * p.S] * p.T
-            for s in range(p.S):
-                for t in range(p.T):
-                    if t < p.BW:
-                        etr_params[t][s] = dict_params["tfunc_avg_etr"][t]
-                        mtrx_params[t][s] = dict_params["tfunc_avg_mtrx"][t]
-                        mtry_params[t][s] = dict_params["tfunc_avg_mtry"][t]
-                    else:
-                        etr_params[t][s] = dict_params["tfunc_avg_etr"][-1]
-                        mtrx_params[t][s] = dict_params["tfunc_avg_mtrx"][-1]
-                        mtry_params[t][s] = dict_params["tfunc_avg_mtry"][-1]
+            # Make all ETRs equal the average
+            etr_params = np.zeros(etr_params.shape)
+            # set shift to average rate
+            etr_params[: p.BW, :, 10] = np.tile(
+                dict_params["tfunc_avg_etr"].reshape(p.BW, 1), (1, p.S)
+            )
+            etr_params[p.BW :, :, 10] = dict_params["tfunc_avg_etr"][-1]
+
+            # # Make all MTRx equal the average
+            mtrx_params = np.zeros(mtrx_params.shape)
+            # set shift to average rate
+            mtrx_params[: p.BW, :, 10] = np.tile(
+                dict_params["tfunc_avg_mtrx"].reshape(p.BW, 1), (1, p.S)
+            )
+            mtrx_params[p.BW :, :, 10] = dict_params["tfunc_avg_mtrx"][-1]
+
+            # # Make all MTRy equal the average
+            mtry_params = np.zeros(mtry_params.shape)
+            # set shift to average rate
+            mtry_params[: p.BW, :, 10] = np.tile(
+                dict_params["tfunc_avg_mtry"].reshape(p.BW, 1), (1, p.S)
+            )
+            mtry_params[p.BW :, :, 10] = dict_params["tfunc_avg_mtry"][-1]
         if p.zero_taxes:
             print("Zero taxes!")
-            etr_params = [[0] * p.S] * p.T
-            mtrx_params = [[0] * p.S] * p.T
-            mtry_params = [[0] * p.S] * p.T
+            etr_params = np.zeros(etr_params.shape)
+            mtrx_params = np.zeros(mtrx_params.shape)
+            mtry_params = np.zeros(mtry_params.shape)
         tax_param_dict = {
             "etr_params": etr_params,
             "mtrx_params": mtrx_params,
             "mtry_params": mtry_params,
-            "taxcalc_version": taxcalc_version,
             "mean_income_data": mean_income_data,
             "frac_tax_payroll": frac_tax_payroll,
         }
@@ -238,16 +297,13 @@ class Calibration:
         """
         This function reads in tax function parameters from pickle
         files.
-
         Args:
             tax_func_path (str): path to pickle with tax function
                 parameter estimates
-
         Returns:
             dict_params (dict): dictionary containing arrays of tax
                 function parameters
             run_micro (bool): whether to estimate tax function parameters
-
         """
         flag = 0
         if os.path.exists(tax_func_path):
@@ -264,7 +320,6 @@ class Calibration:
             except KeyError:
                 pass
             try:
-                p.BW = dict_params["BW"]  # QUICK FIX
                 if p.BW != dict_params["BW"]:
                     print(
                         "Model budget window length is "
@@ -311,12 +366,12 @@ class Calibration:
         dict = {}
         if self.estimate_tax_functions:
             dict.update(self.tax_function_params)
-        if self.estimate_beta:
-            dict["beta_annual"] = self.beta
-        if self.estimate_chi_n:
-            dict["chi_n"] = self.chi_n
-        dict["eta"] = self.eta
-        dict["zeta"] = self.zeta
+        # if self.estimate_beta:
+        #     dict["beta_annual"] = self.beta
+        # if self.estimate_chi_n:
+        #     dict["chi_n"] = self.chi_n
+        # dict["eta"] = self.eta
+        # dict["zeta"] = self.zeta
         dict.update(self.macro_params)
         dict["e"] = self.e
         dict.update(self.demographic_params)
